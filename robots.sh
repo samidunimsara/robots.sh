8e27b83b34ce5e4a5aceaf731ead3e8b8db96dcd
samidunimsara <95490856+samidunimsara@users.noreply.github.com>
2023-02-13 19:05:32 +0530

    Update robots.sh

--- robots.sh
+++ robots.sh
@@ -1,2 +1,7 @@
-httpx -l urls.txt -paths /robots.txt -silent -o robots-url.txt
+#!/usr/bin/bash
+
+echo "Root domain name please :(" 
+read domain
+subfinder -d $domain -all -silent -o urls.txt
+httpx -l urls.txt -path /robots.txt -silent -o robots-url.txt
 for url in $(cat robots-url.txt);do http -b $url | grep 'Disallow' | awk -F ' ' '{print $2}' | cut -c 2- | anew robot-words.txt;done
